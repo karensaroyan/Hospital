02382f3979b210bd1c14c38847591caa0f536808
karen0808 <karensaroyan@yahoo.com>
2021-05-01 20:00:43 +0400

    Create new project

---
+++ hospital/hms/admin/include/sidebar.php
@@ -0,0 +1,212 @@
+<div class="sidebar app-aside" id="sidebar">
+	<div class="sidebar-container perfect-scrollbar">
+
+		<nav>
+
+			<!-- start: MAIN NAVIGATION MENU -->
+			<div class="navbar-title">
+				<span>Գլխավոր</span>
+			</div>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-home"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Կառավարման վահանակ </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Բժիշկներ </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="doctor-specilization.php">
+								<span class="title"> Բժիշկների մասնագիտությունները </span>
+							</a>
+						</li>
+						<li>
+							<a href="add-doctor.php">
+								<span class="title"> Ավելացնել բժիշկների</span>
+							</a>
+						</li>
+						<li>
+							<a href="Manage-doctors.php">
+								<span class="title"> Բժիշկներ </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
+
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Օգտատեր </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="manage-users.php">
+								<span class="title"> Օգտատերերի կարգավորում </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title">
+									Հիվանդներ 
+								</span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="manage-patient.php">
+								<span class="title"> Հիվանդների կարգավորում  </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
+
+				<li>
+					<a href="appointment-history.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-file"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Նշանակման պատմություն </span>
+							</div>
+						</div>
+					</a>
+				</li>
+
+
+
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-files"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Հետադարձ կապ </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="unread-queries.php">
+								<span class="title"> Չընթերցված հարցումներ </span>
+							</a>
+						</li>
+
+						<li>
+							<a href="read-query.php">
+								<span class="title"> Ընթերցված հարցումներ </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
+
+
+
+				<li>
+					<a href="doctor-logs.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Բժիշկների համակարգ մուտքերը </span>
+							</div>
+						</div>
+					</a>
+				</li> 
+
+
+
+				<li>
+					<a href="user-logs.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Օգտատերերի համակարգ մուտքերը</span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<!-- <a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-files"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Զեկույցներ </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="between-dates-reports.php">
+								<span class="title">B/w dates reports </span>
+							</a>
+						</li>
+
+
+
+					</ul>  -->
+				<li>
+					<a href="patient-search.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-search"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Հիվանդների որոնում </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				</li>
+
+			</ul>
+			<!-- end: CORE FEATURES -->
+
+		</nav>
+	</div>
+</div>
---
+++ hospital/hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+					<span class=" text-uppercase">Հիվանդանոցների կառավարման համակարգ &copy;  </span> 2021 | <span>Բոլոր իրավունքները պաշտպանված են </span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
